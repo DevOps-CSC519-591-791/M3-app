cc5399af2e04671024eba5557d7fbc16f6c4540f
Jianfeng <ginfungchan@gmail.com>
2016-11-05 21:37:01 -0400

    add feature flag

--- canary.js
+++ canary.js
@@ -11,21 +11,38 @@ var client = redis.createClient(6379, '127.0.0.1', {})
 var myurl;
 
 function print_hello(req, res){
-	// TODO setting the local url and print that
-	res.write('<html>');
-    res.write('<body>');
-    res.write('Hello! This is the web page for milestone 3 demo.</br></br>');
-    res.write('<b>This instance shows the canary version.</b>');
-    res.write('</body>');
-    res.write('</html>');
+    client.get("flag", function(err,flag){
+        res.write('<html>');
+        res.write('<body>');
+        res.write('Hello! This is the web page for milestone 3 demo.</br></br>');
+        res.write('<b>This instance shows the canary version.</b></br>');
+        if (flag.charAt(0) == '1'){
+            res.write('Feature 1 Enabled.</br>');
+        }
+        if (flag.charAt(1) == '1'){
+            res.write('Feature 2 Enabled.</br>');
+        }
+        if (flag.charAt(2) == '1'){
+            res.write('Feature 3 Enabled.</br>');
+        }
+        if (flag.charAt(3) == '1'){
+         res.write('Feature 4 Enabled.</br>');
+        }
+        if (flag.charAt(4) == '1'){
+         res.write('Feature 5 Enabled.</br>');
+        }
+        res.write('</body>');
+        res.write('</html>');
+        res.end();
+    });
+    
 }
 
 var args = process.argv.slice(2);
 const PORT=args[0]; 
 
 function handleRequest(req, res){
-	print_hello(req, res)
-    res.end();
+    print_hello(req, res);
 }
 
 var server = http.createServer(handleRequest);
@@ -34,3 +51,5 @@ var server = http.createServer(handleRequest);
 server.listen(PORT, function(){
     console.log("Server listening on: http://localhost:%s", PORT);
 });
+
+exports.server = server;
