898964f0bc14672f2b466a5b8472b3b115a5c5ab
drinkbeer <chenjb90@gmail.com>
2016-11-07 22:31:52 -0500

    update monitoring.js

--- monitoring.js
+++ monitoring.js
@@ -1,6 +1,6 @@
 var monitor = require("os-monitor");
 var redis = require("redis")
-
+var http = require("http")
 var client = redis.createClient(6379, '54.234.163.154', {})
 
 
@@ -8,29 +8,45 @@ var client = redis.createClient(6379, '54.234.163.154', {})
 monitor.start(); 
  
 // define handler that will always fire every cycle 
-monitor.on('monitor', function(event) {
-  // console.log('\n')
-  // console.log(event.type, ' This event always happens on each monitor cycle!\n');
-  // console.log(event)
-  console.log('-------------')
-  console.log(event);
-  console.log(event.type)
-  console.log(event.freemem)
-  console.log(event.loadavg[0])
-  console.log('-------------')
-});
+// monitor.on('monitor', function(event) {
+//   // console.log('\n')
+//   // console.log(event.type, ' This event always happens on each monitor cycle!\n');
+//   // console.log(event)
+//   console.log('-------------')
+//   console.log(event);
+//   console.log(event.type)
+//   console.log(event.freemem)
+//   console.log(event.loadavg[0])
+//   console.log('-------------')
+// });
 
-
-var request = require('request');
 
 var url = "http://169.254.169.254/latest/meta-data/public-ipv4";
 
-console.log(url);
-request(url, function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    console.log('request url: '+url);
-  }
+var options = {
+  host: url,
+  port: 80,
+  path: '/upload',
+  method: 'POST'
+};
+
+var req = http.request(options, function(res) {
+  console.log('STATUS: ' + res.statusCode);
+  console.log('HEADERS: ' + JSON.stringify(res.headers));
+  res.setEncoding('utf8');
+  res.on('data', function (chunk) {
+    console.log('BODY: ' + chunk);
+  });
 });
+
+req.on('error', function(e) {
+  console.log('problem with request: ' + e.message);
+});
+
+// write data to request body
+req.write('data\n');
+req.write('data\n');
+req.end();
    
 // // stop monitor 
 // monitor.stop();
@@ -40,4 +56,4 @@ request(url, function (error, response, body) {
  
  
 // use as readable stream 
-monitor.start({stream: true}).pipe(process.stdout);
+// monitor.start({stream: true}).pipe(process.stdout);
