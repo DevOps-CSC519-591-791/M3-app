4ed8b105966fe85118ebe018b360655c5f32e81e
drinkbeer <chenjb90@gmail.com>
2016-11-07 22:59:43 -0500

    update monitoring

--- monitoring.js
+++ monitoring.js
@@ -1,6 +1,7 @@
 var monitor = require("os-monitor");
 var redis = require("redis")
 var http = require("http")
+
 var client = redis.createClient(6379, '54.234.163.154', {})
 
 
@@ -8,29 +9,30 @@ var client = redis.createClient(6379, '54.234.163.154', {})
 monitor.start(); 
  
 // define handler that will always fire every cycle 
-// monitor.on('monitor', function(event) {
-//   // console.log('\n')
-//   // console.log(event.type, ' This event always happens on each monitor cycle!\n');
-//   // console.log(event)
-//   console.log('-------------')
-//   console.log(event);
-//   console.log(event.type)
-//   console.log(event.freemem)
-//   console.log(event.loadavg[0])
-//   console.log('-------------')
+monitor.on('monitor', function(event) {
+  // console.log('\n')
+  // console.log(event.type, ' This event always happens on each monitor cycle!\n');
+  // console.log(event)
+  console.log('-------------')
+  console.log(event);
+  console.log(event.type)
+  console.log(event.freemem)
+  console.log(event.loadavg[0])
+  console.log('-------------')
+});
+
+
+
+// var exec = require('child_process').exec;
+// var cmd = 'curl http://169.254.169.254/latest/meta-data/public-ipv4';
+
+// var publicIP = ''
+// exec(cmd, function(error, stdout, stderr) {
+//   // command output is in stdout
+//   console.log(stdout)
+//   publicIP = stdout
 // });
 
-
-
-var exec = require('child_process').exec;
-var cmd = 'curl http://169.254.169.254/latest/meta-data/public-ipv4';
-
-var publicIP = ''
-exec(cmd, function(error, stdout, stderr) {
-  // command output is in stdout
-  console.log(stdout)
-  publicIP = stdout
-});
 
 
 // // stop monitor 
